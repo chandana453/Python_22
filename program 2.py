b286e80324ef89d0d3a9406320aa7735d07cfe4e
dvsaim020@gmail.com <srinivasdarmisetty@Srinivasdarmisettys-MacBook-Pro.local>
2022-11-16 13:04:06 +0530

    programs

---
+++ program 2.py
@@ -0,0 +1,6 @@
+'''
+Pythonn programto find the second largest number in a list
+solution:
+'''
+list=[12,14,50,6,8]
+print("second largest number in list is:",max(list))
---
+++ program 4.py
@@ -0,0 +1,16 @@
+'''
+python program to split even and odd elements into two lists python
+solution:
+'''
+x=[3,75,8,9,4,388]
+evenl=[]
+oddl=[]
+for i in range(len(x)):
+    if x[i]%2==0:
+        evenl.append(x[i])
+    else:
+        oddl.append(x[i])
+print(evenl)
+print(oddl)
+
+kkk;;;;llllllllll;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
